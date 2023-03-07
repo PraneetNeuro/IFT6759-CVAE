e36c8decd94902e44ff45c7fcccc1f1e2998cb15
tanner-ducharme <tanner.d.ducharme@gmail.com>
2023-03-07 18:14:01 -0500

    created small script for testing wandb integration

---
+++ wandb_test.py
@@ -0,0 +1,33 @@
+import wandb
+import random
+
+art = wandb.Artifact("test-model", type="model")
+art.add_file("test_model_model_weights.pt")
+wandb.log_artifact(art)
+
+# start a new wandb run to track this script
+wandb.init(
+    # set the wandb project where this run will be logged
+    project="sketch-VAE",
+    
+    # track hyperparameters and run metadata
+    config={
+    "learning_rate": 0.02,
+    "architecture": "CNN",
+    "dataset": "CIFAR-100",
+    "epochs": 10,
+    }
+)
+
+# simulate training
+epochs = 10
+offset = random.random() / 5
+for epoch in range(2, epochs):
+    acc = 1 - 2 ** -epoch - random.random() / epoch - offset
+    loss = 2 ** -epoch + random.random() / epoch + offset
+    
+    # log metrics to wandb
+    wandb.log({"acc": acc, "loss": loss})
+    
+# [optional] finish the wandb run, necessary in notebooks
+wandb.finish()
