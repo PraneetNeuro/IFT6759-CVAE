f0e9b2c792879789a9ef21c995967fc7f43147e2
Praneet <praneetquantumx@gmail.com>
2023-03-08 12:44:15 -0500

    Updated templates for AE and VAE

--- VAE_Template.py
+++ VAE_Template.py
@@ -4,8 +4,8 @@ import torch.nn.functional as F
 import numpy as np
 
 
-class AutoEncoder(nn.Module):
-    def __init__(self, input_size, output_size, condition_size=1024, dataset_info={
+class VariationalAutoEncoder(nn.Module):
+    def __init__(self, input_size, output_size, condition_size, dataset_info={
         'source': 'source.npy',
         'target': 'target.npy',
         'condition': 'condition.npy',
@@ -14,8 +14,11 @@ class AutoEncoder(nn.Module):
         'condition': True,
         'condition_type': 'mul',
         'skip_connection': True,
+        'distribution_dim': 128,
     }):
-        super(AutoEncoder, self).__init__()
+        super(VariationalAutoEncoder, self).__init__()
+
+        self.distribution = torch.distributions.Normal(torch.zeros(self.model_config['distribution_dim']), torch.ones(self.model_config['distribution_dim']))
 
         if torch.cuda.is_available():
             self.device = torch.device('cuda')
@@ -36,7 +39,7 @@ class AutoEncoder(nn.Module):
         self.X = torch.from_numpy(self.X).float().view(-1, 1, self.input_size[0], self.input_size[1]).to(self.device)
 
         self.Y = np.load(dataset_info['target']) / 255.0
-        self.Y = torch.from_numpy(self.Y).float().view(-1, 3, self.output_size[0], self.output_size[1]).to(self.device)
+        self.Y = torch.from_numpy(self.Y).float().view(-1, 1, self.output_size[0], self.output_size[1]).to(self.device)
 
         if dataset_info['condition'] is not None:
             self.condition_data = np.load(dataset_info['condition'])
@@ -59,46 +62,56 @@ class AutoEncoder(nn.Module):
         # Encoder layers
 
         # bottleneck and projection layers
-        self.bottle_neck = nn.LazyLinear(condition_size)
-        self.projection_layer = nn.Linear(condition_size, self.projection_size)
-        
-        # Decoder layers
+        self.bottle_neck = nn.Linear(100, condition_size)
+        self.mean = torch.nn.Linear(condition_size, self.model_config['distribution_dim'])
+        self.log_std = torch.nn.Linear(condition_size, self.model_config['distribution_dim'])
 
+        # Decoder layers
+        self.projection_layer = nn.Linear(self.model_config['distribution_dim'], self.projection_size)
 
     def encoder(self, x, condition):
         # Encoder Forward Pass
-
+        
         # high dimensional projection through linear layer
         x = torch.flatten(x)
         x = self.bottle_neck(x)
         if not self.model_config['condition']:
-            if self.model_config['condition_type'] == 'add':
-                condition = torch.zeros_like(x)
-            else:
-                condition = torch.ones_like(x)
+            condition = torch.ones(x.size())
         if self.model_config['condition_type'] == 'add':
             x = torch.add(x, condition)
         else:
             x = torch.mul(x, condition)
-        x = self.projection_layer(x)
-        return x
+        
+        mean = self.mean(x)
+        log_std = self.log_std(x)
+        std = torch.exp(0.5 * log_std)
+        eps = torch.randn_like(std)
+        z = mean + std * eps
+        return z, mean, log_std
     
     def decoder(self, encoded_representation, input):
-        x = encoded_representation.view(-1, 1, self.input_size[0], self.input_size[1])
+        x = self.projection_layer(x)
+        x = encoded_representation.view(-1, self.input_size[0], self.input_size[1])
         if self.model_config['skip_connection']:
             x = torch.cat((x, input), dim=1)
         else:
             x = x
+        # Decoder Forward Pass, make sure the number of channels / kernels is 3, since  the output is a RGB image
 
-        return x
+        # Use the sigmoid function for the final layer
+        pass
     
     def forward(self, input, condition):
-        x = self.encoder(input, condition)
+        z, mean, log_std = self.encoder(input, condition)
         x = self.decoder(x, input)
-        return x
+        return z, mean, log_std, x
     
-    def loss(self, ground_truth, output):
-        return F.mse_loss(ground_truth, output)
+    def loss(self, generated_image, ground_truth, mean, log_std):
+        reconstruction_loss = F.mse_loss(generated_image, ground_truth)
+        latent_distrbution = torch.distributions.Normal(mean, log_std.exp())
+        kl_divergence = F.kl_div(latent_distrbution, self.distribution)
+
+        return reconstruction_loss + kl_divergence
 
     def train(self, epochs, batch_size, save_path):
         optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
@@ -108,8 +121,8 @@ class AutoEncoder(nn.Module):
                 batch_Y = self.train_Y[i:i+batch_size]
                 batch_condition = self.train_condition[i:i+batch_size]
                 optimizer.zero_grad()
-                output = self.forward(batch_X, batch_condition)
-                loss = self.loss(batch_Y, output)
+                z, mean, log_std, output = self.forward(batch_X, batch_condition)
+                loss = self.loss(output, batch_Y, mean, log_std)
                 loss.backward()
                 optimizer.step()
             print(f'Epoch {epoch} Loss: {loss}')
@@ -124,4 +137,3 @@ class AutoEncoder(nn.Module):
     
     def load_model(self, path):
         self.load_state_dict(torch.load(path))
-
